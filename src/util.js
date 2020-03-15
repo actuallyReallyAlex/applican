2aae3d40a4d8a5ab57f192192f0493d186fccf8e
alexlee-dev <alex@alexlee.dev>
2020-03-14 22:21:27 -0700

    Small refactor

--- src/util.js
+++ src/util.js
@@ -4,40 +4,10 @@ import clear from 'clear'
 import figlet from 'figlet'
 import Table from 'cli-table'
 
-import { readFile, stat } from 'fs'
+import { readFile } from 'fs'
 import { join } from 'path'
 
-/**
- * Blank style applied to Boxen.
- */
-const blankBoxenStyle = {
-  borderStyle: {
-    topLeft: ' ',
-    topRight: ' ',
-    bottomLeft: ' ',
-    bottomRight: ' ',
-    horizontal: ' ',
-    vertical: ' '
-  },
-  float: 'center',
-  padding: { top: 0, bottom: 0, right: 1, left: 1 }
-}
-
-/**
- * Default style applied to Boxen.
- */
-const defaultBoxenStyle = {
-  borderColor: 'magentaBright',
-  borderStyle: 'round',
-  float: 'center',
-  padding: { top: 0, bottom: 0, right: 1, left: 1 }
-}
-
-const statusColors = {
-  Accepted: chalk.greenBright,
-  Applied: chalk.yellowBright,
-  Rejected: chalk.redBright
-}
+import { blankBoxenStyle, defaultBoxenStyle, statusColors } from './constants'
 
 /**
  * Uses Figlet to transform your text to ASCII.
